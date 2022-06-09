a97d2a0775447494db42851fdd7416cbb0b3b2b0
Lucas D. Bauer <lucas.d.bauer95@gmail.com>
2022-06-09 15:39:03 -0500

    Add in values to try to first part of exercise

--- DOM-Events/app.js
+++ DOM-Events/app.js
@@ -6,3 +6,12 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+const p = document.createElement('p');                     
+// creates a new div referenced in the variable 'p'
+p.style.color = 'red';                                      
+// adds the indicated style rule
+p.style.cssText = 'color: red; background: white;';          
+// adds several style rules
+p.setAttribute('style', 'color: blue; background: white;');    
+// adds several style rules
