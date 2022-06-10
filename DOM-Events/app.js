2c9d02a736769269f10ee66dc89745a2311c4e4c
Lucas D. Bauer <lucas.d.bauer95@gmail.com>
2022-06-10 09:13:17 -0500

    Setup a new content spot

--- DOM-Events/app.js
+++ DOM-Events/app.js
@@ -12,3 +12,18 @@ const pRedText = document.createElement('p');
 pRedText.style.color = 'Red';
 pRedText.textContent = 'Hey I’m red!';
 container.appendChild(pRedText);
+
+const h3BlueText = document.createElement('h3');
+h3BlueText.style.color = 'Blue';
+h3BlueText.textContent = "I'm a blue h3!";
+container.appendChild(h3BlueText);
+
+const colorContainer = document.createElement('div');
+colorContainer.classList.add('colorContent');
+colorContainer.style.backgroundColor = 'Pink';
+colorContainer.style.border = '1px solid black';
+container.appendChild(colorContainer);
+
+const h1Text =  document.createElement('h1')
+h1Text.textContent = "I'm in a div";
+colorContainer.appendChild(h1Text);
