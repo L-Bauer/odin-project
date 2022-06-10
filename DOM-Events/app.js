12ce2ab8316b084f7b22b38514c2c0dfb90365e6
Lucas D. Bauer <lucas.d.bauer95@gmail.com>
2022-06-10 08:29:28 -0500

    Organize the layout

--- DOM-Events/app.js
+++ DOM-Events/app.js
@@ -7,9 +7,8 @@ content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
 
-const pRedText = document.createElement('p');                     
 // creates a new p referenced in the variable 'p'
+const pRedText = document.createElement('p');                     
 pRedText.style.color = 'Red';
 pRedText.textContent = 'Hey I’m red!';
-
 container.appendChild(pRedText);
